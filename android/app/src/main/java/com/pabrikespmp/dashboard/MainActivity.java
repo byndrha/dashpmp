1258dae1f1d2c048240ae8a669b7ceb31296153e
byndrha <robbyindra2tab@gmail.com>
2026-08-05 01:46:02 +0700

    Install OfflineAwareWebViewClient on app start

--- android/app/src/main/java/com/pabrikespmp/dashboard/MainActivity.java
+++ android/app/src/main/java/com/pabrikespmp/dashboard/MainActivity.java
@@ -2,4 +2,10 @@ package com.pabrikespmp.dashboard;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void load() {
+        super.load();
+        getBridge().setWebViewClient(new OfflineAwareWebViewClient(getBridge()));
+    }
+}
